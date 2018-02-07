cb0f374dabe28b1c66edde31f57a5d0ad4013a80
Victor Amador <vamador@gmail.com>
2018-02-07 11:23:29 -0600

    Add files via upload

---
+++ PressureSensor/PressureSensor.ino
@@ -0,0 +1,32 @@
+/*
+  ReadAnalogVoltage
+  Reads an analog input on pin 0, converts it to voltage, and prints the result to the serial monitor.
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+
+ This example code is in the public domain.
+ */
+int sensorPin = A0;
+int sensorValue = 0;
+
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  sensorValue = analogRead(sensorPin);
+  
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  float voltage = sensorValue * (5 / 1023.0);
+  // print out the value you read:
+  Serial.println("sensorValue");
+  Serial.println(sensorValue);
+  Serial.println("voltage");
+  Serial.println(voltage);
+  Serial.println();
+  delay(1000);
+}
